3bfa27acda6c303a179965a411a251a666880335
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-02-02 16:28:03 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,19 +1,12 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
-dependency "vrp"
 ui_page 'html/index.html'
 client_scripts{ 
- 
   "client.lua"
 }
 
 server_scripts{ 
-
   "server.lua"
 }
-
-
-
 files {
   'html/index.html',
   'html/style.css',
