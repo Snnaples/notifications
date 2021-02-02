6ef0aa989f07cb70c6dcd6b90a4b31a32f4c5cc7
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-02-02 16:27:35 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,6 @@
 RegisterNetEvent("snnaples:Notify")
 AddEventHandler("snnaples:Notify", function(text,time)
-	print(text)
-	print(time)
-	if string.len(text) > 0 and string.len(text) < 100 then
+	if string.len(text) > 0 and string.len(text) < 50 then
 
 		SendNUIMessage(json.encode({
 			type = "ui",
