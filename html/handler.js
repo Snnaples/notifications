b9f55f81ed1dec8dc83479d50a9d43bc169b22e7
Snnaples <68153702+Snnaples@users.noreply.github.com>
2021-02-02 16:26:19 +0200

    Update handler.js

--- html/handler.js
+++ html/handler.js
@@ -1,6 +1,5 @@
 $(function(){
 	window.onload = (e) => {
-        /* 'links' the js with the Nui message from main.lua */
 		window.addEventListener('message', (event) => {
 			function notifD() {
 				document.getElementById("nt").style.display="none";
@@ -8,8 +7,7 @@ $(function(){
 			function notif(text) {
 				document.getElementById("te").innerHTML = text
 				document.getElementById("nt").style.display="block";
-			}
-            //document.querySelector("#logo").innerHTML = " "
+			}    
 			var item = event.data;
 			var json = JSON.parse(item);
 			if (item !== undefined) {
